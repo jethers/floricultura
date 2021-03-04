4f059e5c70ee1364c01425e6bc341d3afa19175d
jethers <jethinho@gmail.com>
2021-03-04 17:50:02 -0300

    novo commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
 </header>
 <body>
-<div align:"center">Compre aqui suas flores:</div>
-<image src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.gazetadopovo.com.br%2F2020%2F11%2F06164250%2Fflores-primavera-pixabay-1-d7bd5fc0-660x372.jpg&imgrefurl=https%3A%2F%2Fwww.gazetadopovo.com.br%2Fhaus%2Fpaisagismo-jardinagem%2F3-formas-12-especies-flores-casa-primavera%2F&tbnid=lQUEt2tWli9tyM&vet=12ahUKEwi7oeO1v5fvAhXiAdQKHYkiBv0QMygcegUIARCTAg..i&docid=XcZNbiieLu4X2M&w=660&h=372&q=flores&ved=2ahUKEwi7oeO1v5fvAhXiAdQKHYkiBv0QMygcegUIARCTAg">
+<h1>Compre aqui suas flores:</h1>
+<image src="./imagens/flores.jfif">
 </body>
 </html>
