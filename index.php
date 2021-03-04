aa9e085e3044498619bd29d383e592f646b1005f
jethers <jethinho@gmail.com>
2021-03-04 17:54:06 -0300

    novo commit

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
 <body>
 <h1>Compre aqui suas flores:</h1>
 <image src="./imagens/flores.jfif">
+<br>
+<A href="https://api.whatsapp.com/send?phone=5511976846005&text=Vendas%20online">Contato Whatsapp</A>
 </body>
 </html>
