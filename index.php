38213b2eb8f433839298be8d6b98fef426cb6844
jethers <jethinho@gmail.com>
2021-03-04 17:41:24 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<header>
+    <title>Floricultura</title>
+    <meta charset="UTF-8">
+</header>
+<body>
+<div align:"center">Compre aqui suas flores:</div>
+<image src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.gazetadopovo.com.br%2F2020%2F11%2F06164250%2Fflores-primavera-pixabay-1-d7bd5fc0-660x372.jpg&imgrefurl=https%3A%2F%2Fwww.gazetadopovo.com.br%2Fhaus%2Fpaisagismo-jardinagem%2F3-formas-12-especies-flores-casa-primavera%2F&tbnid=lQUEt2tWli9tyM&vet=12ahUKEwi7oeO1v5fvAhXiAdQKHYkiBv0QMygcegUIARCTAg..i&docid=XcZNbiieLu4X2M&w=660&h=372&q=flores&ved=2ahUKEwi7oeO1v5fvAhXiAdQKHYkiBv0QMygcegUIARCTAg">
+</body>
+</html>
